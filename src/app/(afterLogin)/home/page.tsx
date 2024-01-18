336eb87c28e9478f1fd8aa23bcc2879752753750
heedaelee <adguy72@gmail.com>
2024-01-18 18:32:06 +0900

    feature : Home탭 만들기 && contextAPI 적용, 20/75강

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,23 @@
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
+
 export default function Home() {
-  return <main>홈페이지</main>;
+  return (
+    <main className={style.main}>
+      {/* TabProvider는 클라이언트 컴포넌트임  */}
+      <TabProvider>
+        <Tab />
+        {/* <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post /> */}
+      </TabProvider>
+    </main>
+  );
 }
